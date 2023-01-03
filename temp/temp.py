4f628bd5c8f7a719362212609d72d1c6d3c7e633
ekert-it <34439886+ekert-it@users.noreply.github.com>
2023-01-03 12:30:21 +0000

    test some features

---
+++ temp/temp.py
@@ -0,0 +1,9 @@
+"""Module providingFunction printing python version."""
+# Ein Kommentar
+import sys
+import pandas as pd
+Liste = [3,4,5,9,330]
+for Element in Liste:
+    print (Element)
+print (Element)
+print (sys.copyright)
